1771501134f3dbc1ddcb4014de67e58517633f73
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2018-11-05 20:16:24 +0530

    state added to app.js

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -7,14 +7,30 @@ const API_KEY='7e3f100c20c2fef0667201c853ba20f0';
 const SAMPLE_API_URL='http://api.openweathermap.org/data/2.5/forecast?q=kolkata,india&APPID=7e3f100c20c2fef0667201c853ba20f0&units=metric';
 
 class App extends React.Component{
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  };
   getWeather = async (e) => {
     e.preventDefault();
     const city=e.target.city.value;
     const country=e.target.country.value;
     try{
-      let api_call = await fetch('http://api.openweathermap.org/data/2.5/forecast?q=kolkata,india&APPID=7e3f100c20c2fef0667201c853ba20f0&units=metric');
+      let api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&APPID=7e3f100c20c2fef0667201c853ba20f0&units=metric`);
       let data= await api_call.json();
       console.log(data);
+      this.setState({
+        temperature: data.list[0].main.temp,
+        city: data.city.name,
+        country: data.city.country,
+        humidity: data.list[0].main.humidity,
+        description: 'Something',
+        error: 'Error ;)'
+      });
     }catch(error){
       console.error(error);
     }
