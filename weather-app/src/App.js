f13352af2af38d1e72c4e0d99e88e484d9731797
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2018-11-05 18:42:39 +0530

    getWeather method fixed in App.js

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -4,23 +4,20 @@ import Form from './components/Form';
 import Weather from './components/Weather';
 
 const API_KEY='7e3f100c20c2fef0667201c853ba20f0';
-const SAMPLE_API_URL='api.openweathermap.org/data/2.5/forecast?q=kolkata,india&APPID=7e3f100c20c2fef0667201c853ba20f0&units=metric';
+const SAMPLE_API_URL='http://api.openweathermap.org/data/2.5/forecast?q=kolkata,india&APPID=7e3f100c20c2fef0667201c853ba20f0&units=metric';
 
 class App extends React.Component{
   getWeather = async (e) => {
     e.preventDefault();
     const city=e.target.city.value;
     const country=e.target.country.value;
-
-    const api_call = await fetch('api.openweathermap.org/data/2.5/forecast?q=manchester,uk&APPID=${API_KEY}&units=metric',{
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
-    });
-    const data= await api_call.json();
-    // const data=JSON.stringify(api_call);
-    console.log(data);
+    try{
+      let api_call = await fetch('http://api.openweathermap.org/data/2.5/forecast?q=kolkata,india&APPID=7e3f100c20c2fef0667201c853ba20f0&units=metric');
+      let data= await api_call.json();
+      console.log(data);
+    }catch(error){
+      console.error(error);
+    }
   }
   render(){
     return(
