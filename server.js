3ae6d30d77b8ba4fbba367037b4e64d835948f91
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2018-11-10 21:47:30 +0530

    server.js and package.json tweaked to meet the requirements of heroku

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express=require('express');
 const app=express();
 const port=process.env.PORT||1998;
 
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 app.listen(port,function(){
     console.log(`Server started at ${port}`);
 });
