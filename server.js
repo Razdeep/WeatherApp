29a35799a9e9626134e4681e3c4900fd4b4cb079
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2018-11-10 21:22:22 +0530

    Port configured for heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const app=express();
-const port=1998;
+const port=process.env.PORT||1998;
 
 app.listen(port,function(){
     console.log(`Server started at ${port}`);
